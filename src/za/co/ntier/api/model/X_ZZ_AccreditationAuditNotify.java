67309e69dd64eb560f10ba20adb234494fc09f70
niraj <nmangukiya@logilite.com>
2026-03-31 18:13:15 +0530

    MQA-269: Model classes for the tables - ZZ_AccreditationAuditNotify and ZZ_AccreditationAuditNotifyLine

---
+++ src/za/co/ntier/api/model/X_ZZ_AccreditationAuditNotify.java
@@ -0,0 +1,510 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package za.co.ntier.api.model;
+
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Properties;
+import org.compiere.model.*;
+
+/** Generated Model for ZZ_AccreditationAuditNotify
+ *  @author iDempiere (generated)
+ *  @version Release 12 - $Id$ */
+@org.adempiere.base.Model(table="ZZ_AccreditationAuditNotify")
+public class X_ZZ_AccreditationAuditNotify extends PO implements I_ZZ_AccreditationAuditNotify, I_Persistent
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20260331L;
+
+    /** Standard Constructor */
+    public X_ZZ_AccreditationAuditNotify (Properties ctx, int ZZ_AccreditationAuditNotify_ID, String trxName)
+    {
+      super (ctx, ZZ_AccreditationAuditNotify_ID, trxName);
+      /** if (ZZ_AccreditationAuditNotify_ID == 0)
+        {
+			setZZ_AccreditationAuditNotify_ID (0);
+			setZZ_AccreditationAuditNotify_UU (null);
+        } */
+    }
+
+    /** Standard Constructor */
+    public X_ZZ_AccreditationAuditNotify (Properties ctx, int ZZ_AccreditationAuditNotify_ID, String trxName, String ... virtualColumns)
+    {
+      super (ctx, ZZ_AccreditationAuditNotify_ID, trxName, virtualColumns);
+      /** if (ZZ_AccreditationAuditNotify_ID == 0)
+        {
+			setZZ_AccreditationAuditNotify_ID (0);
+			setZZ_AccreditationAuditNotify_UU (null);
+        } */
+    }
+
+    /** Standard Constructor */
+    public X_ZZ_AccreditationAuditNotify (Properties ctx, String ZZ_AccreditationAuditNotify_UU, String trxName)
+    {
+      super (ctx, ZZ_AccreditationAuditNotify_UU, trxName);
+      /** if (ZZ_AccreditationAuditNotify_UU == null)
+        {
+			setZZ_AccreditationAuditNotify_ID (0);
+			setZZ_AccreditationAuditNotify_UU (null);
+        } */
+    }
+
+    /** Standard Constructor */
+    public X_ZZ_AccreditationAuditNotify (Properties ctx, String ZZ_AccreditationAuditNotify_UU, String trxName, String ... virtualColumns)
+    {
+      super (ctx, ZZ_AccreditationAuditNotify_UU, trxName, virtualColumns);
+      /** if (ZZ_AccreditationAuditNotify_UU == null)
+        {
+			setZZ_AccreditationAuditNotify_ID (0);
+			setZZ_AccreditationAuditNotify_UU (null);
+        } */
+    }
+
+    /** Load Constructor */
+    public X_ZZ_AccreditationAuditNotify (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+    /** AccessLevel
+      * @return 3 - Client - Org
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
+
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuilder sb = new StringBuilder ("X_ZZ_AccreditationAuditNotify[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	/** Set Date From.
+		@param DateFrom Starting date for a range
+	*/
+	public void setDateFrom (Timestamp DateFrom)
+	{
+		set_Value (COLUMNNAME_DateFrom, DateFrom);
+	}
+
+	/** Get Date From.
+		@return Starting date for a range
+	  */
+	public Timestamp getDateFrom()
+	{
+		return (Timestamp)get_Value(COLUMNNAME_DateFrom);
+	}
+
+	/** Set Date To.
+		@param DateTo End date of a date range
+	*/
+	public void setDateTo (Timestamp DateTo)
+	{
+		set_Value (COLUMNNAME_DateTo, DateTo);
+	}
+
+	/** Get Date To.
+		@return End date of a date range
+	  */
+	public Timestamp getDateTo()
+	{
+		return (Timestamp)get_Value(COLUMNNAME_DateTo);
+	}
+
+	/** Set Document No.
+		@param DocumentNo Document sequence number of the document
+	*/
+	public void setDocumentNo (String DocumentNo)
+	{
+		set_ValueNoCheck (COLUMNNAME_DocumentNo, DocumentNo);
+	}
+
+	/** Get Document No.
+		@return Document sequence number of the document
+	  */
+	public String getDocumentNo()
+	{
+		return (String)get_Value(COLUMNNAME_DocumentNo);
+	}
+
+	/** Set ZZ_AccreditationAuditNotify_ID.
+		@param ZZ_AccreditationAuditNotify_ID ZZ_AccreditationAuditNotify_ID
+	*/
+	public void setZZ_AccreditationAuditNotify_ID (int ZZ_AccreditationAuditNotify_ID)
+	{
+		if (ZZ_AccreditationAuditNotify_ID < 1)
+			set_ValueNoCheck (COLUMNNAME_ZZ_AccreditationAuditNotify_ID, null);
+		else
+			set_ValueNoCheck (COLUMNNAME_ZZ_AccreditationAuditNotify_ID, Integer.valueOf(ZZ_AccreditationAuditNotify_ID));
+	}
+
+	/** Get ZZ_AccreditationAuditNotify_ID.
+		@return ZZ_AccreditationAuditNotify_ID	  */
+	public int getZZ_AccreditationAuditNotify_ID()
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_ZZ_AccreditationAuditNotify_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set ZZ_AccreditationAuditNotify_UU.
+		@param ZZ_AccreditationAuditNotify_UU ZZ_AccreditationAuditNotify_UU
+	*/
+	public void setZZ_AccreditationAuditNotify_UU (String ZZ_AccreditationAuditNotify_UU)
+	{
+		set_ValueNoCheck (COLUMNNAME_ZZ_AccreditationAuditNotify_UU, ZZ_AccreditationAuditNotify_UU);
+	}
+
+	/** Get ZZ_AccreditationAuditNotify_UU.
+		@return ZZ_AccreditationAuditNotify_UU	  */
+	public String getZZ_AccreditationAuditNotify_UU()
+	{
+		return (String)get_Value(COLUMNNAME_ZZ_AccreditationAuditNotify_UU);
+	}
+
+	public org.compiere.model.I_AD_User getZZ_ApprovedBy() throws RuntimeException
+	{
+		return (org.compiere.model.I_AD_User)MTable.get(getCtx(), org.compiere.model.I_AD_User.Table_ID)
+			.getPO(getZZ_ApprovedBy_ID(), get_TrxName());
+	}
+
+	/** Set Approved By.
+		@param ZZ_ApprovedBy_ID Approved By
+	*/
+	public void setZZ_ApprovedBy_ID (int ZZ_ApprovedBy_ID)
+	{
+		if (ZZ_ApprovedBy_ID < 1)
+			set_Value (COLUMNNAME_ZZ_ApprovedBy_ID, null);
+		else
+			set_Value (COLUMNNAME_ZZ_ApprovedBy_ID, Integer.valueOf(ZZ_ApprovedBy_ID));
+	}
+
+	/** Get Approved By.
+		@return Approved By	  */
+	public int getZZ_ApprovedBy_ID()
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_ZZ_ApprovedBy_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Approved Date.
+		@param ZZ_ApprovedDate Approved Date
+	*/
+	public void setZZ_ApprovedDate (Timestamp ZZ_ApprovedDate)
+	{
+		set_Value (COLUMNNAME_ZZ_ApprovedDate, ZZ_ApprovedDate);
+	}
+
+	/** Get Approved Date.
+		@return Approved Date	  */
+	public Timestamp getZZ_ApprovedDate()
+	{
+		return (Timestamp)get_Value(COLUMNNAME_ZZ_ApprovedDate);
+	}
+
+	/** Set Date Not Approved.
+		@param ZZ_Date_Not_Approved Date Not Approved
+	*/
+	public void setZZ_Date_Not_Approved (Timestamp ZZ_Date_Not_Approved)
+	{
+		set_Value (COLUMNNAME_ZZ_Date_Not_Approved, ZZ_Date_Not_Approved);
+	}
+
+	/** Get Date Not Approved.
+		@return Date Not Approved	  */
+	public Timestamp getZZ_Date_Not_Approved()
+	{
+		return (Timestamp)get_Value(COLUMNNAME_ZZ_Date_Not_Approved);
+	}
+
+	/** Set Date Not Recommended.
+		@param ZZ_Date_Not_Recommended Date Not Recommended
+	*/
+	public void setZZ_Date_Not_Recommended (Timestamp ZZ_Date_Not_Recommended)
+	{
+		set_Value (COLUMNNAME_ZZ_Date_Not_Recommended, ZZ_Date_Not_Recommended);
+	}
+
+	/** Get Date Not Recommended.
+		@return Date Not Recommended	  */
+	public Timestamp getZZ_Date_Not_Recommended()
+	{
+		return (Timestamp)get_Value(COLUMNNAME_ZZ_Date_Not_Recommended);
+	}
+
+	/** Set Date Submitted.
+		@param ZZ_Date_Submitted Date Submitted
+	*/
+	public void setZZ_Date_Submitted (Timestamp ZZ_Date_Submitted)
+	{
+		set_Value (COLUMNNAME_ZZ_Date_Submitted, ZZ_Date_Submitted);
+	}
+
+	/** Get Date Submitted.
+		@return Date Submitted	  */
+	public Timestamp getZZ_Date_Submitted()
+	{
+		return (Timestamp)get_Value(COLUMNNAME_ZZ_Date_Submitted);
+	}
+
+	/** Exec Approve = AE */
+	public static final String ZZ_DOCACTION_ExecApprove = "AE";
+	/** Approve/Do Not Approve = AP */
+	public static final String ZZ_DOCACTION_ApproveDoNotApprove = "AP";
+	/** Complete = CO */
+	public static final String ZZ_DOCACTION_Complete = "CO";
+	/** Evaluate = EV */
+	public static final String ZZ_DOCACTION_Evaluate = "EV";
+	/** Final Approval/Do not Approve = FA */
+	public static final String ZZ_DOCACTION_FinalApprovalDoNotApprove = "FA";
+	/** PrepareCEO = PC */
+	public static final String ZZ_DOCACTION_PrepareCEO = "PC";
+	/** Recommend = RE */
+	public static final String ZZ_DOCACTION_Recommend = "RE";
+	/** Re-Submit = RS */
+	public static final String ZZ_DOCACTION_Re_Submit = "RS";
+	/** Submit = S1 */
+	public static final String ZZ_DOCACTION_Submit = "S1";
+	/** System Only (No manual action) = S2 */
+	public static final String ZZ_DOCACTION_SystemOnlyNoManualAction = "S2";
+	/** Submit to Manager Finance Consumables = SC */
+	public static final String ZZ_DOCACTION_SubmitToManagerFinanceConsumables = "SC";
+	/** Submit to SDL Finance Mgr = SD */
+	public static final String ZZ_DOCACTION_SubmitToSDLFinanceMgr = "SD";
+	/** Submit to Snr Mgr LP = SL */
+	public static final String ZZ_DOCACTION_SubmitToSnrMgrLP = "SL";
+	/** Submit to Snr Mgr Ops = SO */
+	public static final String ZZ_DOCACTION_SubmitToSnrMgrOps = "SO";
+	/** Submit to Snr Mgr Projects = SP */
+	public static final String ZZ_DOCACTION_SubmitToSnrMgrProjects = "SP";
+	/** Submit to Snr Mgr QA = SQ */
+	public static final String ZZ_DOCACTION_SubmitToSnrMgrQA = "SQ";
+	/** Submit to Recommender = SR */
+	public static final String ZZ_DOCACTION_SubmitToRecommender = "SR";
+	/** Submit to Snr Mgr SRU = SS */
+	public static final String ZZ_DOCACTION_SubmitToSnrMgrSRU = "SS";
+	/** Submit to Line Manager = SU */
+	public static final String ZZ_DOCACTION_SubmitToLineManager = "SU";
+	/** Update = UP */
+	public static final String ZZ_DOCACTION_Update = "UP";
+	/** Verify = VE */
+	public static final String ZZ_DOCACTION_Verify = "VE";
+	/** Set Document Action.
+		@param ZZ_DocAction Document Action
+	*/
+	public void setZZ_DocAction (String ZZ_DocAction)
+	{
+
+		set_Value (COLUMNNAME_ZZ_DocAction, ZZ_DocAction);
+	}
+
+	/** Get Document Action.
+		@return Document Action	  */
+	public String getZZ_DocAction()
+	{
+		return (String)get_Value(COLUMNNAME_ZZ_DocAction);
+	}
+
+	/** Approved By Manager Finance Consumables = AC */
+	public static final String ZZ_DOCSTATUS_ApprovedByManagerFinanceConsumables = "AC";
+	/** Approved = AP */
+	public static final String ZZ_DOCSTATUS_Approved = "AP";
+	/** Prepared for CEO = CF */
+	public static final String ZZ_DOCSTATUS_PreparedForCEO = "CF";
+	/** Completed = CO */
+	public static final String ZZ_DOCSTATUS_Completed = "CO";
+	/** Draft = DR */
+	public static final String ZZ_DOCSTATUS_Draft = "DR";
+	/** Error Importing = EE */
+	public static final String ZZ_DOCSTATUS_ErrorImporting = "EE";
+	/** Validation Error = ER */
+	public static final String ZZ_DOCSTATUS_ValidationError = "ER";
+	/** Evaluated = EV */
+	public static final String ZZ_DOCSTATUS_Evaluated = "EV";
+	/** Importing = IG */
+	public static final String ZZ_DOCSTATUS_Importing = "IG";
+	/** Imported = IM */
+	public static final String ZZ_DOCSTATUS_Imported = "IM";
+	/** In Progress = IP */
+	public static final String ZZ_DOCSTATUS_InProgress = "IP";
+	/** Not Recommended By Senior Mgr SDR = N1 */
+	public static final String ZZ_DOCSTATUS_NotRecommendedBySeniorMgrSDR = "N1";
+	/** Not Recommended By Senior Mgr Finance = N2 */
+	public static final String ZZ_DOCSTATUS_NotRecommendedBySeniorMgrFinance = "N2";
+	/** Not Recommended By COO = N3 */
+	public static final String ZZ_DOCSTATUS_NotRecommendedByCOO = "N3";
+	/** Not Recommended By CFO = N4 */
+	public static final String ZZ_DOCSTATUS_NotRecommendedByCFO = "N4";
+	/** Not Recommended By CEO = N5 */
+	public static final String ZZ_DOCSTATUS_NotRecommendedByCEO = "N5";
+	/** Not Approved by Snr Manager = NA */
+	public static final String ZZ_DOCSTATUS_NotApprovedBySnrManager = "NA";
+	/** Not Approved By Manager Finance Consumables = NC */
+	public static final String ZZ_DOCSTATUS_NotApprovedByManagerFinanceConsumables = "NC";
+	/** Not Approved By SDL Finance Mgr = ND */
+	public static final String ZZ_DOCSTATUS_NotApprovedBySDLFinanceMgr = "ND";
+	/** Not Approved By IT Manager = NI */
+	public static final String ZZ_DOCSTATUS_NotApprovedByITManager = "NI";
+	/** Not Approved by LM = NL */
+	public static final String ZZ_DOCSTATUS_NotApprovedByLM = "NL";
+	/** Not Recommended = NR */
+	public static final String ZZ_DOCSTATUS_NotRecommended = "NR";
+	/** Not Approved by Snr Admin Finance = NS */
+	public static final String ZZ_DOCSTATUS_NotApprovedBySnrAdminFinance = "NS";
+	/** Pending = PE */
+	public static final String ZZ_DOCSTATUS_Pending = "PE";
+	/** Query = QR */
+	public static final String ZZ_DOCSTATUS_Query = "QR";
+	/** Recommended By Senior Mgr Finance = R1 */
+	public static final String ZZ_DOCSTATUS_RecommendedBySeniorMgrFinance = "R1";
+	/** Recommended By COO = R2 */
+	public static final String ZZ_DOCSTATUS_RecommendedByCOO = "R2";
+	/** Recommended By CFO = R3 */
+	public static final String ZZ_DOCSTATUS_RecommendedByCFO = "R3";
+	/** Recommended By CEO = R4 */
+	public static final String ZZ_DOCSTATUS_RecommendedByCEO = "R4";
+	/** Recommended for Approval = RA */
+	public static final String ZZ_DOCSTATUS_RecommendedForApproval = "RA";
+	/** Recommended = RC */
+	public static final String ZZ_DOCSTATUS_Recommended = "RC";
+	/** Recommended By Senior Mgr SDR = RD */
+	public static final String ZZ_DOCSTATUS_RecommendedBySeniorMgrSDR = "RD";
+	/** Recommended for Evaluation = RE */
+	public static final String ZZ_DOCSTATUS_RecommendedForEvaluation = "RE";
+	/** Submitted to Manager Finance Consumables = SC */
+	public static final String ZZ_DOCSTATUS_SubmittedToManagerFinanceConsumables = "SC";
+	/** Submitted To SDL Finance Mgr = SD */
+	public static final String ZZ_DOCSTATUS_SubmittedToSDLFinanceMgr = "SD";
+	/** Submitted To IT Manager = SI */
+	public static final String ZZ_DOCSTATUS_SubmittedToITManager = "SI";
+	/** Submitted To IT Admin = ST */
+	public static final String ZZ_DOCSTATUS_SubmittedToITAdmin = "ST";
+	/** Submitted = SU */
+	public static final String ZZ_DOCSTATUS_Submitted = "SU";
+	/** Transfer Out = TO */
+	public static final String ZZ_DOCSTATUS_TransferOut = "TO";
+	/** Updated by SDR Admin = UA */
+	public static final String ZZ_DOCSTATUS_UpdatedBySDRAdmin = "UA";
+	/** Uploaded = UP */
+	public static final String ZZ_DOCSTATUS_Uploaded = "UP";
+	/** Delinked = UnSdfOrg */
+	public static final String ZZ_DOCSTATUS_Delinked = "UnSdfOrg";
+	/** Validating = VA */
+	public static final String ZZ_DOCSTATUS_Validating = "VA";
+	/** Verified = VE */
+	public static final String ZZ_DOCSTATUS_Verified = "VE";
+	/** Set Document Status.
+		@param ZZ_DocStatus Document Status
+	*/
+	public void setZZ_DocStatus (String ZZ_DocStatus)
+	{
+
+		set_Value (COLUMNNAME_ZZ_DocStatus, ZZ_DocStatus);
+	}
+
+	/** Get Document Status.
+		@return Document Status	  */
+	public String getZZ_DocStatus()
+	{
+		return (String)get_Value(COLUMNNAME_ZZ_DocStatus);
+	}
+
+	/** Set Recommended Date.
+		@param ZZ_RecommendedDate Recommended Date
+	*/
+	public void setZZ_RecommendedDate (Timestamp ZZ_RecommendedDate)
+	{
+		set_Value (COLUMNNAME_ZZ_RecommendedDate, ZZ_RecommendedDate);
+	}
+
+	/** Get Recommended Date.
+		@return Recommended Date	  */
+	public Timestamp getZZ_RecommendedDate()
+	{
+		return (Timestamp)get_Value(COLUMNNAME_ZZ_RecommendedDate);
+	}
+
+	public org.compiere.model.I_AD_User getZZ_Recommender() throws RuntimeException
+	{
+		return (org.compiere.model.I_AD_User)MTable.get(getCtx(), org.compiere.model.I_AD_User.Table_ID)
+			.getPO(getZZ_Recommender_ID(), get_TrxName());
+	}
+
+	/** Set Recommender.
+		@param ZZ_Recommender_ID Recommender
+	*/
+	public void setZZ_Recommender_ID (int ZZ_Recommender_ID)
+	{
+		if (ZZ_Recommender_ID < 1)
+			set_Value (COLUMNNAME_ZZ_Recommender_ID, null);
+		else
+			set_Value (COLUMNNAME_ZZ_Recommender_ID, Integer.valueOf(ZZ_Recommender_ID));
+	}
+
+	/** Get Recommender.
+		@return Recommender	  */
+	public int getZZ_Recommender_ID()
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_ZZ_Recommender_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	public org.compiere.model.I_AD_User getZZ_Submitter() throws RuntimeException
+	{
+		return (org.compiere.model.I_AD_User)MTable.get(getCtx(), org.compiere.model.I_AD_User.Table_ID)
+			.getPO(getZZ_Submitter_ID(), get_TrxName());
+	}
+
+	/** Set Submitted By.
+		@param ZZ_Submitter_ID Submitted By
+	*/
+	public void setZZ_Submitter_ID (int ZZ_Submitter_ID)
+	{
+		if (ZZ_Submitter_ID < 1)
+			set_Value (COLUMNNAME_ZZ_Submitter_ID, null);
+		else
+			set_Value (COLUMNNAME_ZZ_Submitter_ID, Integer.valueOf(ZZ_Submitter_ID));
+	}
+
+	/** Get Submitted By.
+		@return Submitted By	  */
+	public int getZZ_Submitter_ID()
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_ZZ_Submitter_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+}
